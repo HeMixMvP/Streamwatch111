c74063976b5d230729f4368b8637b134d40955cb
HeMixMvP <44098198+HeMixMvP@users.noreply.github.com>
2018-12-15 21:05:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "c";
 var adminprefix = 'r'
 
-const developers = ["373139395643179010"]
+const developers = ["445973548117524491"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
